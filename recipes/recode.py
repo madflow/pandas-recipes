d5ffa6fec99829b97ddb4f297a4eb82aba9b0198
Florian Reiss <florian.reiss@tivian.de>
2013-12-12 13:29:15 +0100

    Recoding

--- recipes/recode.py
+++ recipes/recode.py
@@ -26,7 +26,6 @@ df = pd.DataFrame({
     'B' : np.random.randint(5, size=10),
 })
 
-
 'Recode into column C with some operations'
 df['C'] = 0.0
 
@@ -38,3 +37,17 @@ print df
 # Print Column Attributes
 print df.dtypes
 
+'Recode with floats'
+df2 = pd.DataFrame({
+    'A' : [1,2,3,4,5]  
+})
+
+df2['B'] = df2['A'].astype(float).replace([1, 2, 3, 4, 5], [1, 0.85, 0.70, 0.55, 0.40])
+
+df2['C'] = df2['A'].map(dict(zip([1, 2, 3, 4, 5], [1, 0.85, 0.70, 0.55, 0.40])))
+
+# Print DataFrame
+print df2
+
+# Print Column Attributes
+print df2.dtypes
